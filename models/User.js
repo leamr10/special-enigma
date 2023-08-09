dcac90ea59b7adebf5d2b4c748367fa19c40f7c3
leamr10 <leamr10@gmail.com>
2023-08-09 18:11:09 -0400

    user models edited

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [/.+@.+\..+/, "Must match an email address"]
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: "thought"
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    }]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function(){
+    return this.friends.length
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
